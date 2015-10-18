64f1589348328d152e67fb422826ca03db559b19
bastard <chrusty@gmail.com>
2015-10-18 15:41:48 +0100

    Figuring out pointers

--- dns/dns.go
+++ dns/dns.go
@@ -1,149 +1,68 @@
 package dns
 
-// import (
-// 	"fmt"
-// 	log "github.com/cihub/seelog"
-// 	"github.com/goamz/goamz/aws"
-// 	"github.com/goamz/goamz/route53"
-// 	"os"
-// 	"strings"
-// 	"time"
-// )
+import (
+	"sync"
+	"time"
 
-// // Lookup the Route53 zone-id for the domain-name we were given:
-// func getRoute53ZoneId(domainName string) string {
+	log "github.com/cihub/seelog"
 
-// 	// Authenticate with AWS:
-// 	awsAuth, err := aws.GetAuth("", "", "", time.Now())
-// 	if err != nil {
-// 		log.Criticalf("[dnsUpdater] Unable to authenticate to AWS! (%s)", err)
-// 		os.Exit(1)
+	types "github.com/chrusty/dns-from-gce/types"
+)
 
-// 	} else {
-// 		log.Debugf("[dnsUpdater] Authenticated to AWS")
-// 	}
+// Configuration object for the DNSUpdater:
+type Config struct {
+	UpdateFrequency    int
+	HostInventory      *types.HostInventory
+	HostInventoryMutex *sync.Mutex
+}
 
-// 	// Make a new EC2 connection:
-// 	log.Debugf("[dnsUpdater] Connecting to Route53 ...")
-// 	route53Connection, err := route53.NewRoute53(awsAuth)
-// 	if err != nil {
-// 		log.Criticalf("[dnsUpdater] Unable to connect to Route53! (%s)", err)
-// 		os.Exit(1)
-// 	}
+// Periodically populate DNS using the host-inventory:
+func Updater(config Config) {
 
-// 	// Submit the request:
-// 	ListHostedZonesResponse, err := route53Connection.ListHostedZones("", 100)
-// 	if err != nil {
-// 		log.Criticalf("[dnsUpdater] Failed to make ListHostedZones call: %v", err)
-// 		os.Exit(2)
-// 	} else {
-// 		log.Debugf("[dnsUpdater] Retreived %d DNS zones.", len(ListHostedZonesResponse.HostedZones))
-// 	}
+	log.Infof("[dnsUpdater] Starting up")
 
-// 	// Go through the responses looking for our zone:
-// 	for _, hostedZone := range ListHostedZonesResponse.HostedZones {
-// 		// Compare the name to the one provided:
-// 		if hostedZone.Name == domainName {
-// 			log.Infof("[dnsUpdater] Found ID (%v) for domain (%v).", hostedZone.Id, domainName)
+	// Run forever:
+	for {
 
-// 			// Split the zone-ID (because they tend to look like "/hostedzone/ZXJHAS123"):
-// 			return strings.Split(hostedZone.Id, "/")[2]
-// 			break
-// 		}
-// 	}
+		// Sleep until the next run:
+		log.Debugf("[dnsUpdater] Sleeping for %vs ...", config.UpdateFrequency)
+		time.Sleep(time.Duration(config.UpdateFrequency) * time.Second)
 
-// 	log.Criticalf("[dnsUpdater] Couldn't find zone-ID for domain (%v)!", domainName)
-// 	os.Exit(1)
-// 	return ""
+		// Lock the host-list (so we don't try to access it when another go-routine is modifying it):
+		log.Tracef("[dnsUpdater] Locking config.HostInventoryMutex...")
+		config.HostInventoryMutex.Lock()
 
-// }
+		log.Tracef("[dnsUpdater] HostIventory: %v", config.HostInventory)
 
-// // Store multi-part file (avoids blowing the memory by loading a huge file):
-// func dnsUpdater() {
+		// See if we actually have any changes to make:
+		if len(config.HostInventory.Environments) > 0 {
 
-// 	log.Infof("[dnsUpdater] Starting up")
+			// Authenticate with GCE:
 
-// 	// Run forever:
-// 	for {
+			// Make a new GCE/DNS connection:
 
-// 		// Sleep until the next run:
-// 		log.Debugf("[dnsUpdater] Sleeping for %vs ...", *dnsupdate)
-// 		time.Sleep(time.Duration(*dnsupdate) * time.Second)
+			// Go through each environment:
+			for environmentName, environment := range config.HostInventory.Environments {
 
-// 		// Lock the host-list (so we don't try to access it when another go-routine is modifying it):
-// 		hostInventoryMutex.Lock()
+				log.Debugf("[dnsUpdater] Creating requests for the '%v' environment ...", environmentName)
 
-// 		// See if we actually have any changes to make:
-// 		if len(hostInventory.Environments) > 0 {
+				// Now iterate over the host-inventory:
+				for dnsRecordName, dnsRecordValue := range environment.DNSRecords {
 
-// 			// Authenticate with AWS:
-// 			awsAuth, err := aws.GetAuth("", "", "", time.Now())
-// 			if err != nil {
-// 				log.Errorf("[dnsUpdater] Unable to authenticate to AWS! (%s)", err)
-// 				continue
+					log.Debug("[dnsUpdater] Record: %v => %v", dnsRecordName, dnsRecordValue)
 
-// 			} else {
-// 				log.Debugf("[dnsUpdater] Authenticated to AWS")
-// 			}
+				}
 
-// 			// Make a new EC2 connection:
-// 			log.Debugf("[dnsUpdater] Connecting to Route53 ...")
-// 			route53Connection, err := route53.NewRoute53(awsAuth)
-// 			if err != nil {
-// 				log.Errorf("[dnsUpdater] Unable to connect to Route53! (%s)", err)
-// 				continue
-// 			}
+			}
 
-// 			// Go through each environment:
-// 			for environmentName, environment := range hostInventory.Environments {
+		} else {
+			log.Info("[dnsUpdater] No DNS changes to make")
+		}
 
-// 				log.Debugf("[dnsUpdater] Creating requests for the '%v' environment ...", environmentName)
+		// Unlock the host-inventory:
+		log.Tracef("[hostInventoryUpdater] Unlocking config.HostInventoryMutex...")
+		config.HostInventoryMutex.Unlock()
 
-// 				// Make an empty batch of changes:
-// 				changes := make([]route53.ResourceRecordSet, 0)
+	}
 
-// 				// Now iterate over the host-inventory:
-// 				for dnsRecordName, dnsRecordValue := range environment.DNSRecords {
-
-// 					// Concatenate the parts together to form the DNS record-name:
-// 					recordName := fmt.Sprintf("%v.%v.%v", dnsRecordName, environmentName, *route53domainName)
-// 					log.Debugf("[dnsUpdater] '%v' => '%v'", recordName, dnsRecordValue)
-
-// 					// Prepare a change-request:
-// 					resourceRecordSet := route53.BasicResourceRecordSet{
-// 						Action: "UPSERT",
-// 						Name:   recordName,
-// 						Type:   "A",
-// 						TTL:    *recordTTL,
-// 						Values: dnsRecordValue,
-// 					}
-
-// 					// Add it to our list of changes:
-// 					changes = append(changes, resourceRecordSet)
-// 				}
-
-// 				// Create a request to modify records:
-// 				changeResourceRecordSetsRequest := route53.ChangeResourceRecordSetsRequest{
-// 					Xmlns:   "https://route53.amazonaws.com/doc/2013-04-01/",
-// 					Changes: changes,
-// 				}
-
-// 				// Submit the request:
-// 				changeResourceRecordSetsResponse, err := route53Connection.ChangeResourceRecordSet(&changeResourceRecordSetsRequest, route53zoneId)
-// 				if err != nil {
-// 					log.Errorf("[dnsUpdater] Failed to make changeResourceRecordSetsResponse call: %v", err)
-// 				} else {
-// 					log.Infof("[dnsUpdater] Successfully updated %d DNS record-sets for %v.%v (Request-ID: %v, Status: %v, Submitted: %v)", len(changes), environmentName, route53domainName, changeResourceRecordSetsResponse.Id, changeResourceRecordSetsResponse.Status, changeResourceRecordSetsResponse.SubmittedAt)
-// 				}
-
-// 			}
-
-// 		} else {
-// 			log.Info("[dnsUpdater] No DNS changes to make")
-// 		}
-
-// 		// Unlock:
-// 		hostInventoryMutex.Unlock()
-// 	}
-
-// }
+}
--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"flag"
-	log "github.com/cihub/seelog"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
-	compute "github.com/chrusty/dns-from-gce/compute"
+
+	log "github.com/cihub/seelog"
+
+	dns "github.com/chrusty/dns-from-gce/dns"
+	hostinventory "github.com/chrusty/dns-from-gce/hostinventory"
+	types "github.com/chrusty/dns-from-gce/types"
 )
 
 var (
-	roleTag            = flag.String("roletag", "role", "EC2 instance tag to derive the 'role' from")
-	environmentTag     = flag.String("environmenttag", "environment", "EC2 instance tag to derive the 'environment' from")
-	recordTTL          = flag.Int("recordttl", 300, "TTL for any DNS records created")
-	hostUpdateFreq     = flag.Int("hostupdate", 60, "How many seconds to sleep between updating the list of hosts from EC2")
-	dnsUpdateFreq      = flag.Int("dnsupdate", 60, "How many seconds to sleep between updating DNS records from the host-list")
-	dnsDomainName      = flag.String("domainname", "domain.com,", "The DNS zone to use (including trailing '.')")
-	hostInventoryMutex sync.Mutex
-	hostInventory      HostInventoryDNSRecords
+	roleMetadataKey        = flag.String("rolekey", "role", "Instance metadata key to derive the 'role' from")
+	environmentMetadataKey = flag.String("environmentkey", "environment", "Instance metadata key to derive the 'environment' from")
+	recordTTL              = flag.Int("recordttl", 300, "TTL for any DNS records created")
+	hostUpdateFreq         = flag.Int("hostupdate", 60, "How many seconds to sleep between updating the list of hosts from GCE")
+	dnsUpdateFreq          = flag.Int("dnsupdate", 60, "How many seconds to sleep between updating DNS records from the host-list")
+	dnsDomainName          = flag.String("domainname", "domain.com,", "The DNS zone to use (including trailing '.')")
+	hostInventoryMutex     sync.Mutex
+	hostInventory          types.HostInventory
 )
 
 func init() {
@@ -30,14 +34,28 @@ func main() {
 	// Make sure we flush the log before quitting:
 	defer log.Flush()
 
-	// Lookup the Route53 zone-id:
-	// route53zoneId = getRoute53ZoneId(*route53domainName)
+	// Configuration object for the HostInventoryUpdater:
+	hostInventoryConfig := &hostinventory.Config{
+		UpdateFrequency:        *hostUpdateFreq,
+		RoleMetadataKey:        *roleMetadataKey,
+		EnvironmentMetadataKey: *environmentMetadataKey,
+		DNSDomainName:          *dnsDomainName,
+		HostInventory:          &hostInventory,
+		HostInventoryMutex:     &hostInventoryMutex,
+	}
 
-	// Update the host-inventory:
-	go compute.HostInventoryUpdater(*hostUpdateFreq, *roleTag, *environmentTag, *dnsDomainName)
+	// Configuration object for the DNSUpdater:
+	dnsConfig := &dns.Config{
+		UpdateFrequency:    *dnsUpdateFreq,
+		HostInventory:      &hostInventory,
+		HostInventoryMutex: &hostInventoryMutex,
+	}
 
-	// Update DNS records for the discovered hosts:
-	// go dnsUpdater()
+	// Run the host-inventory-updater:
+	go hostinventory.Updater(*hostInventoryConfig)
+
+	// Run the dns-updater:
+	go dns.Updater(*dnsConfig)
 
 	// Run until we get a kill-signal:
 	runUntilKillSignal()
